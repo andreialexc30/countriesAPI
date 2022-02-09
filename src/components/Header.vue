8e5b3c3920facbb9b041faa4a2bedd20af35a542
andreialexc30 <andreialexx30@gmail.com>
2022-02-09 18:19:17 +0200

    Credits span to h1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="countriesAPI_header">
-        <h1 class="countriesAPI_header--title">countriesAPI</h1>
+        <h1 class="countriesAPI_header--title">countriesAPI <span class="credits">built by Andrei Caia</span></h1>
         <form class="countriesAPI_header--form">
             <input type="text" placeholder="Type in country, code or capital" class="countriesAPI_form--search">
             <button type="submit" @click.prevent="fetchData" class="countriesAPI_form--btn">Search</button>
