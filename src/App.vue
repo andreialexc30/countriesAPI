1efa235098a2ba6871ddb451113d46ef165e4d51
andreialexc30 <andreialexx30@gmail.com>
2022-02-09 18:07:49 +0200

    Removed router-view related code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
 <Header :fetchData="fetchData"
 :valid="valid" />
-  <!-- <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div> -->
-  <router-view/>
   <section class="countriesAPI_filters">
     <Filters :filterRegion="filterRegion" />
   </section>
   <section class="countriesAPI_displayed">
     <h1 v-if="ifCountry" class="countriesAPI_displayed--warning">Could not find the country you were looking for. Please try again.</h1>
-    <CountryCard :modalData="modalData"
-    :modalCountry="modalCountry"
-    :sortedArray="sortedArray" />
+    <ul class="countriesAPI_displayed--list">
+        <CountryCard :modalData="modalData"
+        :modalCountry="modalCountry"
+        :sortedArray="sortedArray" />
+    </ul>
   </section>
 </template>
 
